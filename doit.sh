22fcafccf1fa0c62030efe8c0dc21280e7ef7b76
Uli Hitzel <uli.hitzel@gmail.com>
2021-09-28 13:44:14 +0000

    changed 1632836654

--- doit.sh
+++ doit.sh
@@ -1,6 +1,8 @@
 while [ 1 ]
 do
-	out=$(date +%s).txt
+	p=$(date +%Y)/$(date +%m)/$(date +%d)/$(date +%H)
+	mkdir -p $p
+	out=$p/$(date +%s).txt
 	curl "http://api.open-notify.org/iss-now.json" > $out
 	git add -A
 	git commit -m "changed $(date +%s)"
